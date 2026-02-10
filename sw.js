d0db24a74e4eafcadf2508a156852c4b11b3905a
jalallelrumii-png <jalallelrumii@gmail.com>
2026-02-10 17:23:23 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,9 @@
-const CACHE_NAME = "boss-store-v1";
-const assets = [
-  "./",
-  "./index.html",
-  "https://cdn.tailwindcss.com",
-  "https://unpkg.com/lucide@latest"
-];
-
-// Install Service Worker
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assets);
-    })
-  );
-});
-
-// Fetching
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+rules_version = '2';
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /{document=**} {
+      allow read, write: if true;
+    }
+  }
+}
 
